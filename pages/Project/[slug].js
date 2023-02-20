300d09e440d912f2789e7bdcf7e377baf1d64141
nmstudios <125855067+nmstudios@users.noreply.github.com>
2023-02-20 18:05:10 +0000

    updated api routes

--- pages/Project/[slug].js
+++ pages/Project/[slug].js
@@ -187,7 +187,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const project = (await fetchAPI(`/projects?slug=${params.slug}`))[0];
+  const project =[];
   return {
     props: {
       project,
